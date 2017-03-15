5ea6c6c41265b3498f14a1e7782c6bc499f76715
clarenswd <clarens007@gmail.com>
2017-03-15 14:08:38 +1000

    arrays

--- herewe.go
+++ herewe.go
@@ -19,4 +19,43 @@ func main(){
         case 28 : fmt.Println("eighteen plus ten")
         default : fmt.Println("non recognized param")
     }
+
+
+    //arrays
+
+
+    var favNums2[5] float64;
+    favNums2[0] = 163;
+    favNums2[1] = 1.3;
+    favNums2[2] = 1.63;
+    favNums2[3] = 13;
+    favNums2[4] = 3;
+
+    fmt.Println(favNums2[3])
+
+
+    favNums3 := [5] float64{1,3,2,4,5};
+    fmt.Println("printing array vals with index")
+    for i,value := range favNums3{
+        fmt.Println(value, i)
+    }
+    fmt.Println("printing array vals with NO index")
+
+    for _,value := range favNums3{
+        fmt.Println(value)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
